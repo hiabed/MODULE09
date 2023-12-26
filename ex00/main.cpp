6e4773a73db6b99b99215733b72300885b743e5f
Mohammed-Abed Hassani-Idrissi <mhassani@e1r11p5.1337.ma>
2023-12-26 17:21:01 +0100

    about to make big change

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,5 +1,17 @@
 #include "BitcoinExchange.hpp"
 
+void check_val(float value)
+{
+    if (value < 0)
+    {
+        std::cout << "Error: not a positive number.\n";
+    }
+    else if (value > 1000)
+    {
+        std::cout << "Error: too large a number.\n";
+    }
+}
+
 float get_value(std::string &iline)
 {
     int i = 0;
@@ -43,35 +55,30 @@ int main(int ac, char **av)
     getline(InputFile, iline); // to skip the first line of av[1] file;
     if (iline != "date | value")
         std::cerr << "Error: bad input => " << iline << std::endl;
+    std::map<std::string, float> data_m;
+    std::map<std::string, float>::iterator found;
     while(getline(InputFile, iline))
     {
         std::string date = iline.substr(0, 10);
         float value = get_value(iline);
-        if (value < 0)
-        {
-            std::cout << "Error: not a positive number.\n";
-            continue;
-        }
-        else if (value > 1000)
-        {
-            std::cout << "Error: too large a number.\n";
-            continue;
-        }
+        check_val(value);
         std::map<std::string, float> m;
         m.insert(std::pair<std::string, float>(date, value));
-        std::map<std::string, float>::iterator iBeg = m.begin();
-        getline(data, dline); // to skip the first line of dataBase;
-        while(getline(data, dline))
-        {
-            std::string data_date = dline.substr(0, 10);
-            float data_value = get_data_value(dline);
-            std::map<std::string, float> data_m;
-            data_m.insert(std::pair<std::string, float>(data_date, data_value));
-            // std::map<std::string, float>::iterator dBeg = data_m.begin();
-            // std::map<std::string, float>::iterator dEnd = data_m.end();
-            std::map<std::string, float>::iterator found = data_m.find(iBeg->first);
-            std::cout << "the value found is:" << found->second << std::endl;
-        }
+
+        // getline(data, dline); // to skip the first line of dataBase;
+        // std::string data_date = dline.substr(0, 10);
+        // float data_value = get_data_value(dline);
+        // data_m.insert(std::pair<std::string, float>(data_date, data_value));
+        // found = data_m.find(iBeg->first);
+        // if(iBeg->first == "2009-02-01")
+        // {
+        //     std::cout << "the value found is: " << found->first << std::endl;
+        //     return 0;
+        // }
+        // else
+        // {
+        //     std::cout << "value not found.\n";
+        // }
     }
     InputFile.close();
     data.close();
