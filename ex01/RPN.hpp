03e5c4211bb43db2c20039f88bb16b69bae3f774
Mohammed-Abed Hassani-Idrissi <mhassani@e1r12p3.1337.ma>
2023-12-27 20:08:19 +0100

    ex01 start

---
+++ ex01/RPN.hpp
@@ -0,0 +1,16 @@
+#ifndef RPN_HPP
+#define RPN_HPP
+
+#include <iostream>
+#include <algorithm>
+
+class RPN
+{
+public:
+    RPN();
+    RPN(const RPN &other);
+    RPN &operator=(const RPN &other);
+    ~RPN();
+};
+
+#endif
