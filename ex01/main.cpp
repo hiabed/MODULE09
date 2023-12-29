cd714d2a71af220cfcf3c389092d8ce2972a999d
Mohammed-Abed Hassani-Idrissi <mhassani@e1r10p5.1337.ma>
2023-12-29 18:22:15 +0100

    ex01 done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -15,19 +15,35 @@ int main(int ac, char **av)
         return 0;
     }
     std::stack<int> st;
-    // int result = 0;
+    int result = 0;
+    int array[2];
     for (int i = 0; av[1][i]; i++)
     {
         if(isdigit(av[1][i]))
-        {
             st.push(atoi(&av[1][i]));
-            std::cout << st.top() << std::endl;
-        }
         else if (is_operator(av[1][i]))
         {
-            st.push(av[1][i]);
-            std::cout << st.top() << std::endl;
+            if(st.size() < 2)
+            {
+                std::cerr << "Error\n";
+                return 0;
+            }
+            array[0] = st.top();
+            st.pop();
+            array[1] = st.top();
+            st.pop();
+            if (av[1][i] == '+')
+                result = array[1] + array[0];
+            else if (av[1][i] == '-')
+                result = array[1] - array[0];
+            else if (av[1][i] == '*')
+                result = array[1] * array[0];
+            else if (av[1][i] == '/')
+                result = array[1] / array[0];
+            st.push(result);
         }
     }
+    if (st.size() == 1)
+        std::cout << result << std::endl;
     return 0;
 }
