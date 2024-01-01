8eb82c3fa7f996edc74ec94f82d9a2a1848b5825
Mohammed-Abed Hassani-Idrissi <mhassani@e1r9p5.1337.ma>
2024-01-01 17:29:17 +0100

    some parsing handled

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,6 +7,16 @@ int is_operator(char av1)
     return 0;
 }
 
+int parse_input(char *av1)
+{
+    for (int i = 0; av1[i]; i++)
+    {
+        if (av1[i] != '+' && av1[i] != '-' && av1[i] != '*' && av1[i] != '/' && av1[i] != ' ' && !isdigit(av1[i]))
+            return 0;
+    }
+    return 1;
+}
+
 int main(int ac, char **av)
 {
     if (ac != 2)
@@ -14,15 +24,22 @@ int main(int ac, char **av)
         std::cerr << "Error: bad number of arguments.\n";
         return 0;
     }
+    if (!parse_input(av[1]))
+    {
+        std::cerr << "Error\n";
+        return 0;
+    }
     std::stack<int> st;
     int result = 0;
     int array[2];
+    int op = 0;
     for (int i = 0; av[1][i]; i++)
     {
         if(isdigit(av[1][i]))
             st.push(atoi(&av[1][i]));
         else if (is_operator(av[1][i]))
         {
+            op = 1;
             if(st.size() < 2)
             {
                 std::cerr << "Error\n";
@@ -43,7 +60,9 @@ int main(int ac, char **av)
             st.push(result);
         }
     }
-    if (st.size() == 1)
+    if (st.size() == 1 && op)
         std::cout << result << std::endl;
+    else
+        std::cerr << "Error\n";
     return 0;
 }
